f15603cafd1724db9b7d47b387d11d937e6d1113
talel <talelmejri8@gmail.com>
2023-01-13 22:27:41 +0100

    first commit

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  files: ['src/views/**/*.vue'],
+  rules: {
+    'vue/multi-word-component-names': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'md',
+      },
+});
