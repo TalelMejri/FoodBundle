5d176c0f334d6c1eada5d7ebcffee85d9dd0dbe6
talel <talelmejri8@gmail.com>
2023-01-14 18:40:20 +0100

    add page loader

---
+++ frontend/src/components/preloader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="content">
+             <img width="200px" height="200px" src="../assets/loader.gif" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .content{
+        background-color:  #fff;
+        height: 100vh;
+        z-index: 9999999999999;
+        position: fixed;
+        top:0;
+        left:0;
+        width: 100%;
+        display: flex;
+        min-height: 100vh;
+        justify-content: center;
+        align-items: center; 
+    }
+    
+</style>
