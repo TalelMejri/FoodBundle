e4a6ef0eb61d22a30ff9d4c63d310b2ee31de5ea
talel <talelmejri8@gmail.com>
2023-01-15 13:29:17 +0100

    fix

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -20,6 +20,7 @@ _axios.interceptors.request.use(
   function(config) {
     // Do something before request is sent
     return config;
+    
   },
   function(error) {
     // Do something with request error
