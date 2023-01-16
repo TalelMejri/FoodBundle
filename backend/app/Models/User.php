cdb03f8156a387b52c7ca65e0131caa9850549fc
talel <talelmejri8@gmail.com>
2023-01-16 08:01:34 +0100

    add attribut in modal

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,19 +11,21 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    public $timestamps=false;
+    //public $timestamps=false;
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
         'name',
         'email',
         'password',
         'lastname',
         'numero_tlf',
-        'Photo'
+        'Photo',
+        'Isadmin'
     ];
 
     /**
