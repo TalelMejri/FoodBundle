ca1a354bf4b60aae05abfc5a64cee1b2950987c6
talel <talelmejri8@gmail.com>
2023-01-17 02:09:29 +0100

    add api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\auth\LoginUser;
 use App\Http\Controllers\Auth\RegisterUser;
+use App\Http\Controllers\product\ProductController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +25,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['prefix'=>'/auth'],function(){
     Route::post('/registerUser',[RegisterUser::class,'RegisterUser']);
     Route::post('/loginUser',[LoginUser::class,'LoginUser']);
+});
+
+Route::group(['prefix'=>'/user'],function(){
+    Route::get('/countUser',[UserController::class,'coutUser']);
+    Route::get('/getuser',[UserController::class,'index']);
+    Route::delete('/deleteUser/{id}',[UserController::class,'destroy']);
+});
+
+Route::group(['prefix'=>'/product'],function(){
+    Route::post('/addCategory',[ProductController::class,'addCategory']);
 });
