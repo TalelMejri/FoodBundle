5dde1a46deb44a4269b21e62b5e318c3fdae13e9
talel <talelmejri8@gmail.com>
2023-01-20 08:12:49 +0100

    add fonctions in service

--- frontend/src/services/GererProduct/GererProduct.js
+++ frontend/src/services/GererProduct/GererProduct.js
@@ -1,16 +1,13 @@
 import "@/plugins/axios";
 import Axios from "axios";
 
-
-
 export default{
-    addCategory(product){
 
+    addCategory(product){
         /*var data=new FormData();
         data.append('name',product.name);
         data.append('option',product.option);
         data.append('file',product.file);*/
-        
         const config={
             headers:{
                 "Content-Type":"Undefined",
@@ -19,16 +16,53 @@ export default{
         //console.log(product.option);
         return Axios.post('product/addCategory',product);
     },
+
     getAllTypeCategory(){
         return Axios.get('product/AllTypeCategorie');
     },
-    getAllCategorieOptions(search){
-        return Axios.get(`product/GetOptionForCategorie?${search ? "search="+search : ""}`);
+
+    getAllCategorieOptions(search,page){
+        return Axios.get(`product/GetOptionForCategorie?${search ? "search="+search : ""}&page=${page}`);
     },
+
     deleteCategory(id){
         return Axios.delete('product/deleteCategory/'+id);
     },
+
     deleteOption(id){
         return Axios.delete('product/deleteOption/'+id);
+    },
+
+    UpdateOption(id,option){
+        return Axios.post('product/UpdateOption/'+id,option);
+    },
+    
+    UpdateCategory(id,category){
+        return Axios.post('product/UpdateCategory/'+id,category);
+    },
+
+    findCategoryByIid(id){
+        return Axios.get('product/findCategoryByIid/'+id);
+    },
+
+    findOptionByIid(id){
+        return Axios.get('product/findOptionByIid/'+id);
+    },
+
+    addOption(option){
+        return Axios.post('product/addOption',option);
+    },
+
+    CountCategory(){
+        return Axios.get('product/CountCategory');
+    },
+
+    getOptionByIdCategory(user){
+        return Axios.get('product/getOptionByIdCategory/'+user.id);   
+    },
+
+    Add_Product(Product){
+        return Axios.post('product/Add_Product',Product);   
     }
+
 }
