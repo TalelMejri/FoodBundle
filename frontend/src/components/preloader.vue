cb1f74a270e2c1b4901239e64ac6c13c4a71660b
talel <talelmejri8@gmail.com>
2023-01-21 18:19:58 +0100

    fix preloader

--- frontend/src/components/preloader.vue
+++ frontend/src/components/preloader.vue
@@ -1,20 +1,28 @@
 <template>
-    <div  class="content">
+    <div v-if="preloader"  class="preloader">
         <img width="200px" height="200px" src="../assets/loader.gif" alt="">
     </div>
 </template>
-
 <script>
 export default{
-    name:"loader"
+    name:"loader",
+    mounted(){
+        setTimeout(()=>this.preloader=false,1);
+    },
+    data(){
+        return{
+            preloader:true
+        }
+    }
 }
 </script>
 
 <style scoped>
-    .content{
+    .preloader{
         background-color:  #fff;
         height: 100vh;
         position: fixed;
+        z-index:99999999;
         top:0;
         left:0;
         width: 100vw;
