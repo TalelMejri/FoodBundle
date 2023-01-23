e523a19134f01e6d6763321e6db9c9484084fe11
talel <talelmejri8@gmail.com>
2023-01-23 09:06:15 +0100

    add panierView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,6 +8,7 @@ import AllFavoriteProduct from '../views/ProfilClient/AllFavoriteView.vue'
 import dashboard from '../views/ProfilAdmin/dashboard/DashboardView.vue'
 import editProfil from "@/views/ProfilClient/EditProfilView.vue"
 import ConsulteMenuView from "@/views/Menu/ConsulteMenuView.vue"
+import PanierView from "@/views/Menu/PanierView.vue"
 Vue.use(VueRouter)
 
 const routes = [
@@ -15,6 +16,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/PanierView',
+    name: 'PanierView',
+    component: PanierView
   },
   {
     path: '/ConsulteMenuView/:id',
---
+++ frontend/src/views/Menu/PanierView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default{
+    name:'panier',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
