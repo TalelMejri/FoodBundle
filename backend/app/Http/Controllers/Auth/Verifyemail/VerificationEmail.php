ba975a35b765433795d830093a319b61b1db5fa3
talel <talelmejri8@gmail.com>
2023-02-17 15:34:36 +0100

    verification controller

---
+++ backend/app/Http/Controllers/Auth/Verifyemail/VerificationEmail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\auth\Verifyemail;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class VerificationEmail extends Controller
+{
+
+    public function verify_email($email)
+    {
+        $user = User::where('email',$email)->First();
+        if (!$user->hasVerifiedEmail()) {
+            $user->markEmailAsVerified();
+               return response()->json(['data'=>"Email verifé avec succès"],200);
+        } else {
+                return response()->json(['data'=>"Email est déja verifé"],200);
+        }
+    }
+
+    public function renvoyer($email)
+    {
+        $user = User::where('email', $email)->first();
+        if ($user->hasVerifiedEmail()) {
+            return response()->json(["data" => "email est déja vérifé"], 400);
+        }
+        $user->sendEmailVerify();
+        return response()->json(["data" => "lien de vérification envoé avec succès"], 200);
+    }
+}
