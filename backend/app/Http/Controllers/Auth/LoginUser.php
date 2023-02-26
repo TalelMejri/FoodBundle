870e44767f8fbecb3d3e08df11b843674cd76474
talel <talelmejri8@gmail.com>
2023-02-26 23:24:27 +0100

    clear default

--- backend/app/Http/Controllers/Auth/LoginUser.php
+++ backend/app/Http/Controllers/Auth/LoginUser.php
@@ -10,39 +10,40 @@ use Illuminate\Support\Facades\Auth;
 class LoginUser extends Controller
 {
 
-    public function LoginUser(Request $request){
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
-            $user=Auth::user();
+    public function LoginUser(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
             if (!$user->hasVerifiedEmail()) {
-                return response()->json(['data'=>"votre email n'est pas vérifié",'status'=>"email"],401);
+                return response()->json(['data' => "votre email n'est pas vérifié", 'status' => "email"], 401);
             }
-            $token=$user->createToken('api_token')->plainTextToken;
-            $respnose=[
-                'token'=>$token,
-                'user'=>$user,
-                'isAdmin'=>$user->Isadmin
+            $token = $user->createToken('api_token')->plainTextToken;
+            $respnose = [
+                'token' => $token,
+                'user' => $user,
+                'isAdmin' => $user->Isadmin
             ];
-            return response()->json(['data'=>$respnose],200);
-        }else{
-            return response()->json(['data'=>"Utilisateur non trouvé",'status'=>"user"],401);
+            return response()->json(['data' => $respnose], 200);
+        } else {
+            return response()->json(['data' => "Utilisateur non trouvé", 'status' => "user"], 401);
         }
     }
 
-    public function exist_email($email){
-        $user=User::where('email',$email)->first();
-        if($user){
+    public function exist_email($email)
+    {
+        $user = User::where('email', $email)->first();
+        if ($user) {
             return response()->json([
                 'success'   => false,
-                'message'   => 'User already exists',
+                'message'   => 'email already exists',
                 'data'      => []
             ], 201);
         } else {
             return response()->json([
                 'success'   => true,
-                'message'   => 'User not exists',
+                'message'   => 'email not exists',
                 'data'      => []
             ]);
         }
     }
-
 }
