39d6cd6bf5a7320cc2c92463bf128db4ea4175a1
talel <talelmejri8@gmail.com>
2023-03-21 22:03:47 +0100

    fix window.pusher

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,29 @@
-import Vue from 'vue'
-import './plugins/axios'
-import App from './App.vue'
-import router from './router'
-import store from './store/StoreAuth'
-import 'aos/dist/aos.css';
+import Vue from "vue";
+import "./plugins/axios";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store/StoreAuth";
+import "aos/dist/aos.css";
 import Vuelidate from "./plugins/Vuelidate";
-import { createPinia, PiniaVuePlugin } from 'pinia'
-import {interceptors} from '../src/global/interceptors.js'
-import vuetify from './plugins/vuetify'
-Vue.use(PiniaVuePlugin)
-export const pinia = createPinia()
-Vue.config.productionTip = false
+import { createPinia, PiniaVuePlugin } from "pinia";
+import { interceptors } from "../src/global/interceptors.js";
+import vuetify from "./plugins/vuetify";
+Vue.use(PiniaVuePlugin);
+export const pinia = createPinia();
+Vue.config.productionTip = false;
 interceptors();
+
+import Echo from "laravel-echo";
+window.Pusher = require("pusher-js");
+window.Echo = new Echo({
+  broadcaster: "pusher",
+  key: process.env.VUE_APP_WEBSOCKETS_KEY,
+  wsHost: process.env.VUE_APP_WEBSOCKETS_SERVER,
+  wsPort: 6001,
+  cluster: "mt1",
+  forceTLS: false,
+  disableStats: true,
+});
 
 new Vue({
   Vuelidate,
@@ -19,10 +31,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
-
-
-
-
-
+  render: (h) => h(App),
+}).$mount("#app");
