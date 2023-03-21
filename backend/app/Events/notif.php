b36f501b9e28d75abb40aea83b90c9cc589cd0b2
talel <talelmejri8@gmail.com>
2023-03-21 21:58:41 +0100

    event notif

---
+++ backend/app/Events/notif.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Console\Scheduling\Event;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class notif  implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $notif;
+    public function __construct($notif)
+    {
+        $this->notif=$notif;
+    }
+    public function broadcastWith()
+    {
+        return [
+            'message'=>$this->notif
+        ];
+    }
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('public');
+    }
+}
