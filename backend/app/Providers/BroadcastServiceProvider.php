77df7e42e83d176a94fa0cdb201d3a2e063fb825
talel <talelmejri8@gmail.com>
2023-03-21 21:59:19 +0100

    clear default

--- backend/app/Providers/BroadcastServiceProvider.php
+++ backend/app/Providers/BroadcastServiceProvider.php
@@ -15,7 +15,8 @@ class BroadcastServiceProvider extends ServiceProvider
     public function boot()
     {
         Broadcast::routes();
-
+        //Broadcast::routes(["prefix" => "api", "middleware" => ["auth:sanctum"]]);
+        //Broadcast::routes(['middleware' => ['auth:api']]);
         require base_path('routes/channels.php');
     }
 }
